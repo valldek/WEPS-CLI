62e89b82abb589a6da910597f2944b3a443b418e
valldek <valldek@gmail.com>
2020-04-10 23:29:21 +0200

    App Mount fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React from 'react';
 
+import { fetchSummary } from './helpers';
+
 import styles from './App.module.css';
 
 export class App extends React.Component {
+  state = {
+    data: {},
+    countries: [],
+    selectedData: {},
+    selectedCountry: 'global'
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchSummary();
+
+    const countries = fetchedData.Countries.map((country) => {
+      return {
+        countryCode: country.CountryCode,
+        countryName: country.Country,
+        countrySlug: country.Slug,
+      }
+    })
+
+    const global = fetchedData.Global;
+    global.Date = fetchedData.Date;
+
+    this.setState({
+      countries,
+      data: fetchedData,
+      selectedData: global
+    });
+  }
+
   render() {
     return (
       <h1>App</h1>
---
+++ src/componenets/Card/Card.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import styles from './Card.module.css';
+
+export const Card = () => {
+  return null;
+}
